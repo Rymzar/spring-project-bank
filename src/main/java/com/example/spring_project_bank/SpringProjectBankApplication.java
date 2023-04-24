fcf6d1dc2d3b7ee4975c0e09bb6bbbb02c3243e9
Nikta Zemlianskikh <rymzar@gmail.com>
2023-04-24 15:54:09 +0300

    first commit

---
+++ src/main/java/com/example/spring_project_bank/SpringProjectBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_project_bank/SpringProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
